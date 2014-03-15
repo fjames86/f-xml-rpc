c9367d00c1862b3a5671f5a8b753ea6b890181f3
Frank James <frank.a.james@gmail.com>
2014-03-15 15:46:46 +0000

    adding authorization special var

--- f-xml-rpc.lisp
+++ f-xml-rpc.lisp
@@ -3,24 +3,25 @@
   (:use :cl)
   (:nicknames :fxpc)
   (:export #:call-xml-rpc-server
-	   #:encode-xml-rpc-call
-	   #:xml-rpc-call
-	   
-	   #:make-xml-rpc-server 
-	   #:start-xml-rpc-server
-	   #:stop-xml-rpc-server
-	   #:export-system-methods 
-
-	   #:xml-rpc-error
-
-	   #:make-xml-rpc-struct
-	   #:xml-rpc-struct
-	   #:xml-rpc-struct-p
-	   #:xml-rpc-struct-member
-	   #:xml-rpc-struct-alist 
-
-	   #:define-xml-rpc-export
-	   #:define-xml-rpc-call))
+		   #:encode-xml-rpc-call
+		   #:xml-rpc-call
+		   #:*authorization*
+		   
+		   #:make-xml-rpc-server 
+		   #:start-xml-rpc-server
+		   #:stop-xml-rpc-server
+		   #:export-system-methods 
+		   
+		   #:xml-rpc-error
+		   
+		   #:make-xml-rpc-struct
+		   #:xml-rpc-struct
+		   #:xml-rpc-struct-p
+		   #:xml-rpc-struct-member
+		   #:xml-rpc-struct-alist 
+		   
+		   #:define-xml-rpc-export
+		   #:define-xml-rpc-call))
 
 (defpackage :f-xml-rpc-exports)
 
@@ -36,10 +37,10 @@
   ((code :initarg :code :reader xml-rpc-fault-code)
    (string :initarg :string :reader xml-rpc-fault-string))
   (:report (lambda (condition stream)
-	     (format stream
-		     "XML-RPC fault with message '~a' and code ~d."
-		     (xml-rpc-fault-string condition)
-		     (xml-rpc-fault-code condition))))
+			 (format stream
+					 "XML-RPC fault with message '~a' and code ~d."
+					 (xml-rpc-fault-string condition)
+					 (xml-rpc-fault-code condition))))
   (:documentation "This condition is thrown when the XML-RPC server returns a fault"))
 
 (setf (documentation 'xml-rpc-fault-code 'function) "Get the code from an XML-RPC fault")
@@ -49,10 +50,10 @@
   ((place :initarg :code :reader xml-rpc-error-place)
    (data :initarg :data :reader xml-rpc-error-data))
   (:report (lambda (condition stream)
-	     (format stream
-		     "XML-RPC error ~a at ~a."
-		     (xml-rpc-error-data condition)
-		     (xml-rpc-error-place condition))))
+			 (format stream
+					 "XML-RPC error ~a at ~a."
+					 (xml-rpc-error-data condition)
+					 (xml-rpc-error-place condition))))
   (:documentation "This condition is thrown when an XML-RPC protocol error occurs"))
 
 (setf (documentation 'xml-rpc-error-place 'function)
@@ -67,13 +68,13 @@
   (multiple-value-bind (second minute hour date month year)
       (decode-universal-time time)
     (format stream
-	    "~d~2,'0d~2,'0dT~2,'0d:~2,'0d:~2,'0d"
-	    year
-	    month
-	    date
-	    hour
-	    minute
-	    second)))
+			"~d~2,'0d~2,'0dT~2,'0d:~2,'0d:~2,'0d"
+			year
+			month
+			date
+			hour
+			minute
+			second)))
 
 (defun iso8601->universal-time (string)
   "Convert string in the XML-RPC variant of ISO8601 to a Common Lisp universal time"
@@ -81,12 +82,12 @@
     (when (< (length string) 9)
       (error "~s is to short to represent an iso8601" string))
     (setf year (parse-integer string :start 0 :end 4)
-	  month (parse-integer string :start 4 :end 6)
-	  date (parse-integer string :start 6 :end 8))
+		  month (parse-integer string :start 4 :end 6)
+		  date (parse-integer string :start 6 :end 8))
     (when (and (>= (length string) 17) (char= #\T (char string 8)))
       (setf hour (parse-integer string :start 9 :end 11)
-	    minute (parse-integer string :start 12 :end 14)
-	    second (parse-integer string :start 15 :end 17)))
+			minute (parse-integer string :start 12 :end 14)
+			second (parse-integer string :start 15 :end 17)))
     (encode-universal-time second minute hour date month year)))
 
 (defstruct (xml-rpc-time (:print-function print-xml-rpc-time))
@@ -101,8 +102,8 @@
 (defun print-xml-rpc-time (xml-rpc-time stream depth)
   (declare (ignore depth))
   (format stream
-	  "#<XML-RPC-TIME ~a>"
-	  (universal-time->iso8601 (xml-rpc-time-universal-time xml-rpc-time))))
+		  "#<XML-RPC-TIME ~a>"
+		  (universal-time->iso8601 (xml-rpc-time-universal-time xml-rpc-time))))
 
 (defun xml-rpc-time (&optional (universal-time (get-universal-time)))
   "Create a new XML-RPC-TIME struct with the universal time specified, defaulting to now"
@@ -124,8 +125,8 @@
 (defun print-xml-literal (xml-literal stream depth)
   (declare (ignore depth))
   (format stream
-	  "#<XML-LITERAL \"~a\" >"
-	  (xml-literal-content xml-literal)))
+		  "#<XML-LITERAL \"~a\" >"
+		  (xml-literal-content xml-literal)))
 
 (defun xml-literal (content)
   "Create a new XML-LITERAL struct with the specified content."
@@ -147,9 +148,9 @@
   (format stream "#<XML-RPC-STRUCT :KEYS ~S>"
 		  (mapcar #'car (xml-rpc-struct-alist xml-element))))
 
-;(defun get-xml-rpc-struct-member (struct member)
-;  "Get the value of a specific member of an XML-RPC-STRUCT"
-;  (cdr (assoc member (xml-rpc-struct-alist struct))))
+										;(defun get-xml-rpc-struct-member (struct member)
+										;  "Get the value of a specific member of an XML-RPC-STRUCT"
+										;  (cdr (assoc member (xml-rpc-struct-alist struct))))
 
 (defun (setf xml-rpc-struct-member) (value struct member)
   "Set the value of a specific member of an XML-RPC-STRUCT"
@@ -166,7 +167,7 @@
        (if (integerp key)
 		   (setf obj (elt obj key))
 		   (setf obj (cdr (assoc key (xml-rpc-struct-alist obj)))))
-       finally (return obj)))
+	 finally (return obj)))
 
 (defun xml-rpc-struct (&rest args)
   "Create a new XML-RPC-STRUCT from the arguments: alternating member names and values"
@@ -174,26 +175,26 @@
     (error "~s must contain an even number of elements" args))
   (let (alist)
     (loop (if (null args)
-	      (return)
-	      (push (cons (pop args) (pop args)) alist)))
+			  (return)
+			  (push (cons (pop args) (pop args)) alist)))
     (make-xml-rpc-struct :alist alist)))
 
 (defun xml-rpc-struct-equal (struct1 struct2)
   "Compare two XML-RPC-STRUCTs for equality"
   (equal (xml-rpc-struct-alist struct1)
-	 (xml-rpc-struct-alist struct2)))
+		 (xml-rpc-struct-alist struct2)))
 
 ;;; encoding support
 
 (defun encode-xml-rpc-struct (struct stream)
   (princ "<struct>" stream)
   (mapcar (lambda (pair)
-	    (destructuring-bind (key . value) pair
-	      (princ "<member>" stream)
-	      (format stream "<name>~a</name>" key) ; assuming name contains no special characters
-	      (encode-xml-rpc-value value stream)
-	      (princ "</member>" stream)))
-	  (xml-rpc-struct-alist struct))
+			(destructuring-bind (key . value) pair
+			  (princ "<member>" stream)
+			  (format stream "<name>~a</name>" key) ; assuming name contains no special characters
+			  (encode-xml-rpc-value value stream)
+			  (princ "</member>" stream)))
+		  (xml-rpc-struct-alist struct))
   (princ "</struct>" stream))
 
 (defun encode-xml-rpc-array (sequence stream)
@@ -204,32 +205,32 @@
 (defun encode-xml-rpc-value (arg stream)
   (princ "<value>" stream)
   (cond ((or (null arg) (eq arg t))
-	 (princ "<boolean>" stream)
-	 (princ (if arg 1 0) stream)
-	 (princ "</boolean>" stream))
-	((or (stringp arg) (symbolp arg))
-	 (princ "<string>" stream)
-	 (s-xml:print-string-xml (string arg) stream)
-	 (princ "</string>" stream))
-	((integerp arg) (format stream "<int>~d</int>" arg))
-	((floatp arg) (format stream "<double>~f</double>" arg))	
-	((and (arrayp arg)
-	      (= (array-rank arg) 1)
-	      (subtypep (array-element-type arg)
-			'(unsigned-byte 8)))
-	 (princ "<base64>" stream)
-	 (cl-base64:usb8-array-to-base64-stream arg stream)
-	 (princ "</base64>" stream))
-	((xml-rpc-time-p arg)
-	 (princ "<dateTime.iso8601>" stream)
-	 (universal-time->iso8601 (xml-rpc-time-universal-time arg) stream)
-	 (princ "</dateTime.iso8601>" stream))
+		 (princ "<boolean>" stream)
+		 (princ (if arg 1 0) stream)
+		 (princ "</boolean>" stream))
+		((or (stringp arg) (symbolp arg))
+		 (princ "<string>" stream)
+		 (s-xml:print-string-xml (string arg) stream)
+		 (princ "</string>" stream))
+		((integerp arg) (format stream "<int>~d</int>" arg))
+		((floatp arg) (format stream "<double>~f</double>" arg))	
+		((and (arrayp arg)
+			  (= (array-rank arg) 1)
+			  (subtypep (array-element-type arg)
+						'(unsigned-byte 8)))
+		 (princ "<base64>" stream)
+		 (cl-base64:usb8-array-to-base64-stream arg stream)
+		 (princ "</base64>" stream))
+		((xml-rpc-time-p arg)
+		 (princ "<dateTime.iso8601>" stream)
+		 (universal-time->iso8601 (xml-rpc-time-universal-time arg) stream)
+		 (princ "</dateTime.iso8601>" stream))
         ((xml-literal-p arg)
          (princ (xml-literal-content arg) stream))
-	((or (listp arg) (vectorp arg)) (encode-xml-rpc-array arg stream))
-	((xml-rpc-struct-p arg) (encode-xml-rpc-struct arg stream))
-	;; add generic method call
-	(t (error "cannot encode ~s" arg)))
+		((or (listp arg) (vectorp arg)) (encode-xml-rpc-array arg stream))
+		((xml-rpc-struct-p arg) (encode-xml-rpc-struct arg stream))
+		;; add generic method call
+		(t (error "cannot encode ~s" arg)))
   (princ "</value>" stream))
 
 (defun encode-xml-rpc-args (args stream)
@@ -285,53 +286,53 @@
 (defun decode-xml-rpc-finish-element (name attributes parent-seed seed)
   (declare (ignore attributes))
   (cons (case name
-	  ((:|int| :|i4|)
-	   (if *decode-value-types*
-		   :int
-		   (parse-integer seed)))
-	  (:|double|
-		(if *decode-value-types*
-			:double
-			(read-from-string seed)))
-	  (:|boolean|
-		(if *decode-value-types*
-			:boolean
-			(= 1 (parse-integer seed))))
-	  (:|string|
-		(if *decode-value-types*
-			:string
-			(if (null seed) "" seed)))
-	  (:|dateTime.iso8601|
-		(if *decode-value-types*
-			:datetime
-			(xml-rpc-time (iso8601->universal-time seed))))
-	  (:|base64|
-		(if (null seed)
-			(make-array 0 :element-type '(unsigned-byte 8))
-			(cl-base64:base64-string-to-usb8-array seed)))
-	  (:|array|	(car seed))
-	  (:|data| (nreverse seed))  ; potential problem with empty data i.e. <data>\n</data> parsed as "\n"
-	  (:|value| (if (stringp seed) seed (car seed)))
-	  (:|struct| (make-xml-rpc-struct :alist seed))
-	  (:|member| (cons (cadr seed) (car seed)))
-	  (:|name| (intern seed :keyword))
-	  (:|params| (nreverse seed))  ; potential problem with empty params <params>\n</params> parsed as "\n"
-	  (:|param| (car seed))
-	  (:|fault| (make-condition 'xml-rpc-fault
-				    :string (xml-rpc-struct-member (car seed) :|faultString|)
-				    :code (xml-rpc-struct-member (car seed) :|faultCode|)))
-	  (:|methodName| seed)
-	  (:|methodCall| (let ((pair (nreverse seed)))
-			   (cons (car pair) (cadr pair))))
-	  (:|methodResponse| (car seed)))
-	parent-seed))
+		  ((:|int| :|i4|)
+		   (if *decode-value-types*
+			   :int
+			   (parse-integer seed)))
+		  (:|double|
+			(if *decode-value-types*
+				:double
+				(read-from-string seed)))
+		  (:|boolean|
+			(if *decode-value-types*
+				:boolean
+				(= 1 (parse-integer seed))))
+		  (:|string|
+			(if *decode-value-types*
+				:string
+				(if (null seed) "" seed)))
+		  (:|dateTime.iso8601|
+			(if *decode-value-types*
+				:datetime
+				(xml-rpc-time (iso8601->universal-time seed))))
+		  (:|base64|
+			(if (null seed)
+				(make-array 0 :element-type '(unsigned-byte 8))
+				(cl-base64:base64-string-to-usb8-array seed)))
+		  (:|array|	(car seed))
+		  (:|data| (nreverse seed))  ; potential problem with empty data i.e. <data>\n</data> parsed as "\n"
+		  (:|value| (if (stringp seed) seed (car seed)))
+		  (:|struct| (make-xml-rpc-struct :alist seed))
+		  (:|member| (cons (cadr seed) (car seed)))
+		  (:|name| (intern seed :keyword))
+		  (:|params| (nreverse seed))  ; potential problem with empty params <params>\n</params> parsed as "\n"
+		  (:|param| (car seed))
+		  (:|fault| (make-condition 'xml-rpc-fault
+									:string (xml-rpc-struct-member (car seed) :|faultString|)
+									:code (xml-rpc-struct-member (car seed) :|faultCode|)))
+		  (:|methodName| seed)
+		  (:|methodCall| (let ((pair (nreverse seed)))
+						   (cons (car pair) (cadr pair))))
+		  (:|methodResponse| (car seed)))
+		parent-seed))
 
 ;; fixes issue with empty params, data 
 (defun decode-xml-rpc-text (string seed)
   (declare (ignore seed))
   (if (> (length (string-trim '(#\Space #\Newline #\Return)
-			      string))
-	 0)
+							  string))
+		 0)
       string
       nil))
 
@@ -376,7 +377,7 @@
 
 The HOST, PORT and URL of the server. 
 
-Set SSL for communication over https
+Set SSL to T for communication over https.
 
 Set AUTHORIZATION to a 2-element list of username and password to use basic authorization.
 
@@ -398,8 +399,12 @@ See the drakma documentation for using proxy servers, PROXY PROXY-BASIC-AUTHORIZ
 	  (declare (ignore ruri headers stream must-close))
 	  ;; if the response comes in as an array of char codes then 
 	  ;; convert it to a string
-	  (when (arrayp body)
-	    (setf body (map 'string #'code-char body)))
+	  (when (and (not (stringp body)) (arrayp body))
+	    (setf body (map 'string (lambda (c)
+								  (if (integerp c)
+									  (code-char c)
+									  c))
+						body)))
 
 	  (if (= status-code 200)
 		  (with-input-from-string (bstream body)
@@ -418,20 +423,20 @@ See the drakma documentation for using proxy servers, PROXY PROXY-BASIC-AUTHORIZ
 (defun describe-server (&key (host *xml-rpc-host*) (port *xml-rpc-port*) (url *xml-rpc-url*))
   "Tries to describe a remote server using system.* methods"
   (dolist (method (xml-rpc-call (encode-xml-rpc-call "system.listMethods")
-				:host host
-				:port port
-				:url url))
+								:host host
+								:port port
+								:url url))
     (format t
-	    "Method ~a ~a~%~a~%~%"
-	    method
-	    (xml-rpc-call (encode-xml-rpc-call "system.methodSignature" method)
-			  :host host
-			  :port port
-			  :url url)
-	    (xml-rpc-call (encode-xml-rpc-call "system.methodHelp" method)
-			  :host host
-			  :port port
-			  :url url))))
+			"Method ~a ~a~%~a~%~%"
+			method
+			(xml-rpc-call (encode-xml-rpc-call "system.methodSignature" method)
+						  :host host
+						  :port port
+						  :url url)
+			(xml-rpc-call (encode-xml-rpc-call "system.methodHelp" method)
+						  :host host
+						  :port port
+						  :url url))))
 
 
 ;;; server API
@@ -474,7 +479,7 @@ See the drakma documentation for using proxy servers, PROXY PROXY-BASIC-AUTHORIZ
                  (xml-rpc-fault
                   #'(lambda (c)
                       (format-debug "Call failed with ~a~%" c)
-		      (return-from handle-xml-rpc-call
+					  (return-from handle-xml-rpc-call
                         (encode-xml-rpc-fault (xml-rpc-fault-string c)
                                               (xml-rpc-fault-code c)))))
                  (error
@@ -505,6 +510,8 @@ See the drakma documentation for using proxy servers, PROXY PROXY-BASIC-AUTHORIZ
    (package :initarg :package :initform *xml-rpc-package*))
   (:default-initargs :address *xml-rpc-host*))
 
+(defvar *authorization* nil)
+
 (defun acceptor-handler (a request)
   (let ((encoded (map 'string #'code-char
 					  (hunchentoot:raw-post-data))))
@@ -517,7 +524,8 @@ See the drakma documentation for using proxy servers, PROXY PROXY-BASIC-AUTHORIZ
 		  ((not ahandler)
 		   (handle-xml-rpc-call encoded (slot-value a 'package)))
 		  ((and username (funcall ahandler username password))
-		   (handle-xml-rpc-call encoded (slot-value a 'package)))
+		   (let ((*authorization* (list username password)))
+			 (handle-xml-rpc-call encoded (slot-value a 'package))))
 		  (t
 		   (setf (hunchentoot:return-code*) hunchentoot:+http-forbidden+)
 		   ""))))))
@@ -553,25 +561,25 @@ See the drakma documentation for using proxy servers, PROXY PROXY-BASIC-AUTHORIZ
 	   :auth-handler auth-handler
 	   :package package)))
 
-(defparameter *xml-rpc-acceptor* nil)
+(defvar *xml-rpc-acceptor* nil)
 
 (defun start-xml-rpc-server (&key acceptor 
-			       (port *xml-rpc-port*)
-			       ssl-certificate-file ssl-privatekey-file
-			       ssl-privatekey-password
-			       auth-handler (package *xml-rpc-package*))
+							 (port *xml-rpc-port*)
+							 ssl-certificate-file ssl-privatekey-file
+							 ssl-privatekey-password
+							 auth-handler (package *xml-rpc-package*))
   (if acceptor
 	  (hunchentoot:start acceptor)
 	  (progn
 		(setf *xml-rpc-acceptor* 
 		      (make-xml-rpc-acceptor :port port
-					     :ssl-certificate-file ssl-certificate-file
-					     :ssl-privatekey-file ssl-privatekey-file
-					     :ssl-privatekey-password ssl-privatekey-password
-					     :auth-handler auth-handler 
-					     :package package))
+									 :ssl-certificate-file ssl-certificate-file
+									 :ssl-privatekey-file ssl-privatekey-file
+									 :ssl-privatekey-password ssl-privatekey-password
+									 :auth-handler auth-handler 
+									 :package package))
 		(hunchentoot:start *xml-rpc-acceptor*))))
-	
+
 ;; stop the server 
 
 (defun stop-xml-rpc-server (&optional acceptor)
@@ -599,10 +607,10 @@ See the drakma documentation for using proxy servers, PROXY PROXY-BASIC-AUTHORIZ
   `(defun ,name ,params
      (xml-rpc-call (encode-xml-rpc-call ,(if method method (symbol-name name))
 										,@params)
-       :url ,url
-       :host ,host
-	   :authorization ,authorization
-       :port ,port)))
+				   :url ,url
+				   :host ,host
+				   :authorization ,authorization
+				   :port ,port)))
 
 
 
@@ -610,28 +618,28 @@ See the drakma documentation for using proxy servers, PROXY PROXY-BASIC-AUTHORIZ
   (let (exports)
     (do-symbols (var *xml-rpc-package*)
       (if (fboundp var)
-	  (push (symbol-name var) exports)))
+		  (push (symbol-name var) exports)))
     exports))
 
 (defun |system.methodSignature| (method)
   (let ((fn (find-xml-rpc-method method *xml-rpc-package*)))
     (if fn
-	(third (function-lambda-expression (symbol-function fn)))
-	(error 'xml-rpc-fault :code -32601
+		(third (function-lambda-expression (symbol-function fn)))
+		(error 'xml-rpc-fault :code -32601
                :string (format nil "Method ~A not found." method)))))
 
 (defun |system.methodHelp| (method)
   (let ((fn (find-xml-rpc-method method *xml-rpc-package*)))
     (if fn
-	(documentation fn 'function)
-	(error 'xml-rpc-fault :code -32601
+		(documentation fn 'function)
+		(error 'xml-rpc-fault :code -32601
                :string (format nil "Method ~A not found." method)))))
 
 (defun export-system-methods (&optional (package *xml-rpc-package*))
   (loop for sym in '(|system.listMethods| |system.methodSignature| |system.methodHelp|)
-       do (handler-case (import sym package)
-	    (package-error () 
-	      (format-debug "Unable to import ~S to package ~S" sym package)))))
+	 do (handler-case (import sym package)
+		  (package-error () 
+			(format-debug "Unable to import ~S to package ~S" sym package)))))
 
 
 
